95c2a0a3e82787e06990e200a76ccb02f684302b
sanchex-22 <david21.cs70@gmail.com>
2025-06-17 10:42:27 -0500

    pdf generation chrommium version

--- email-templates/generatePdfBuffer.tsx
+++ email-templates/generatePdfBuffer.tsx
@@ -1,12 +1,12 @@
-import chromium from "chrome-aws-lambda";
-import puppeteer from "puppeteer-core";
+import chromium from 'chrome-aws-lambda';
+import puppeteer from 'puppeteer-core';
 
 export async function generatePdfBuffer(htmlContent) {
   const isServerless = !!process.env.AWS_EXECUTION_ENV || process.env.VERCEL === "1";
 
   const executablePath = isServerless
     ? await chromium.executablePath
-    : require("puppeteer").executablePath();
+    : require('puppeteer').executablePath();
 
   const browser = await puppeteer.launch({
     args: isServerless ? chromium.args : [],
@@ -17,11 +17,10 @@ export async function generatePdfBuffer(htmlContent) {
   });
 
   const page = await browser.newPage();
-  await page.setBypassCSP(true);
-  await page.setContent(htmlContent, { waitUntil: "networkidle0" });
+  await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
 
   const pdfBuffer = await page.pdf({
-    format: "a4",
+    format: 'a4',
     printBackground: true,
     margin: { top: '20px', right: '20px', bottom: '20px', left: '20px' },
   });
